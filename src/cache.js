028be2cf0db47ff74f8cae59702fb81582f5cf8e
Alejo Rodriguez <arodriguez@sophilabs.com>
2016-07-12 23:26:26 -0300

    add support for es7 decorators

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,4 @@
-export default function cache(time) {
+function cache(time) {
   return function(f) {
     let result_cache = {};
 
@@ -22,5 +22,16 @@ export default function cache(time) {
 
       return result;
     }
+  }
+}
+
+export default function(time) {
+  return function(target, property, descriptor) {
+    if (typeof target == 'function') {
+      return cache(time)(target);
+    }
+
+    descriptor.value = cache(time)(descriptor.value);
+    return descriptor;
   }
 }
